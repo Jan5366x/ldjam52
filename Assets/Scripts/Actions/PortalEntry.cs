ecdbd410ea6366f9641f7ea787d01f5fd568c2de
Mehran <mehran.net1@gmail.com>
2023-01-08 12:38:16 +0100

    Added scene change to portal entry script

--- Assets/Scripts/Actions/PortalEntry.cs
+++ Assets/Scripts/Actions/PortalEntry.cs
@@ -3,11 +3,15 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PortalEntry : MonoBehaviour
 {
     [AllowsNull]
     public GameObject afterEffectPrefab;
+
+    [AllowsNull]
+    public string nextSceneName;
 
     void Start()
     {
@@ -31,8 +35,25 @@ public class PortalEntry : MonoBehaviour
             if (afterEffectPrefab != null)
                 Instantiate(afterEffectPrefab, transform.position, Quaternion.identity);
 
+            if (!string.IsNullOrWhiteSpace(nextSceneName))
+                StartCoroutine(LoadNextSceneAsync());
+
             Destroy(gameObject);
         }
+    }
+
+    IEnumerator LoadNextSceneAsync()
+    {
+        //Debug.Log($"Loading next scene: {nextSceneName}");
+        var loadOperation = SceneManager.LoadSceneAsync(nextSceneName);
+        // Do we need loading screen?
+
+        while (!loadOperation.isDone)
+        {
+            yield return null;
+        }
+
+        //Debug.Log($"Loaded next scene: {nextSceneName}");
     }
 
     private void SwitchWorlds() =>
