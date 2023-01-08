5316afa95b743091b971b25ee2840213e0dae91b
Jan Schwien <jan5366@gmail.com>
2023-01-08 16:09:43 +0100

    World swap debugging

---
+++ Assets/Scripts/DebugWorldToggle.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class DebugWorldToggle : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.LogWarning("Debug World Toggle exist ... remove before release!!!!"); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButton("Fire1"))
+        {
+            GlobalVariables.world = GlobalVariables.world == GlobalVariables.World.NormalDimention
+                ? GlobalVariables.World.OtherDimention
+                : GlobalVariables.World.NormalDimention;
+            
+            Debug.LogWarning("Debug World Toggle triggered!!!!"); 
+        }
+    }
+}
