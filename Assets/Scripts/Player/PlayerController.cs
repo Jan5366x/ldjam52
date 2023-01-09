37f34492276eaa844de8528ec2c79c9e0357b2c1
kormarun <kormarun@gmail.com>
2023-01-09 12:02:59 +0100

    use fixed update for physics stuff

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -36,7 +36,7 @@ public class PlayerController : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         timeSinceLastMove += Time.deltaTime;
         timeSinceLastJump += Time.deltaTime;
